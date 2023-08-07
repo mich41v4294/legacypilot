325cd3fae409b07a6201b8cc624b992f1a540be1
AdamSBlack <18472768+AdamSBlack@users.noreply.github.com>
2023-08-07 19:10:10 +0100

    disable gps event + update code for new op

--- selfdrive/car/volkswagen/carcontroller.py
+++ selfdrive/car/volkswagen/carcontroller.py
@@ -107,5 +107,5 @@ class CarController:
     self.gra_acc_counter_last = CS.gra_stock_values["COUNTER"]
     self.frame += 1
     
-    can_sends.append(volkswagencan.pla_control(self.packer_pt, ext_bus, 300, True))
+    can_sends.append(self.CCS.pla_control(self.packer_pt, ext_bus, 300, True))
     return new_actuators, can_sends, self.eps_timer_soft_disable_alert
--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -756,8 +756,8 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
   # is thrown. This can mean a service crashed, did not broadcast a message for
   # ten times the regular interval, or the average interval is more than 10% too high.
   EventName.commIssue: {
-    ET.SOFT_DISABLE: soft_disable_alert("Communication Issue between Processes"),
-    ET.NO_ENTRY: comm_issue_alert,
+    #ET.SOFT_DISABLE: soft_disable_alert("Communication Issue between Processes"),
+    #ET.NO_ENTRY: comm_issue_alert,
   },
   EventName.commIssueAvgFreq: {
     ET.SOFT_DISABLE: soft_disable_alert("Low Communication Rate between Processes"),
