804f02e5ed9e26c26b1c717f3d3a201693dba605
AdamSBlack <18472768+AdamSBlack@users.noreply.github.com>
2023-08-07 18:04:10 +0100

    oops

--- panda/tests/hitl/8_spi.py
+++ panda/tests/hitl/8_spi.py
@@ -4,7 +4,7 @@ import random
 from unittest.mock import patch
 
 from panda import Panda
-from panda.python.spi import PandaSpiNackResponse
+from panda.python.spi import PandaProtocolMismatch, PandaSpiNackResponse
 
 pytestmark = [
   pytest.mark.test_panda_types((Panda.HW_TYPE_TRES, ))
@@ -19,7 +19,19 @@ class TestSpi:
     mocker.stop(spy)
 
   @pytest.mark.expected_logs(2)
-  def test_protocol_version(self, p):
+  def test_protocol_version_check(self, p):
+    for bootstub in (False, True):
+      p.reset(enter_bootstub=bootstub)
+      with patch('panda.python.spi.PandaSpiHandle.PROTOCOL_VERSION', return_value="abc"):
+        # list should still work with wrong version
+        assert p._serial in Panda.list()
+
+        # connect but raise protocol error
+        with pytest.raises(PandaProtocolMismatch):
+          Panda(p._serial)
+
+  @pytest.mark.expected_logs(2)
+  def test_protocol_version_data(self, p):
     for bootstub in (False, True):
       p.reset(enter_bootstub=bootstub)
       v = p._handle.get_protocol_version()
@@ -32,8 +44,6 @@ class TestSpi:
 
       bstub = v[13]
       assert bstub == (0xEE if bootstub else 0xCC)
-
-      assert v[14:] == b"\x01"
 
   def test_all_comm_types(self, mocker, p):
     spy = mocker.spy(p._handle, '_wait_for_ack')
